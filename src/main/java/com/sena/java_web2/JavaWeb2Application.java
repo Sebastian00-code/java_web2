1a8eaf3a3c50c17161ed454b161f052b9142139e
Sebastian <sebastianargote99@gmail.com>
2025-08-10 11:23:51 -0500

    Initial commit

---
+++ src/main/java/com/sena/java_web2/JavaWeb2Application.java
@@ -0,0 +1,13 @@
+package com.sena.java_web2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWeb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/java_web2/JavaWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.java_web2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
